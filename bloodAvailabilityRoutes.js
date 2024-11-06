d7355ffcb760b8195356b02f6a39c6144f2d555a
padmaja48 <147294816+padmaja48@users.noreply.github.com>
2024-11-06 20:03:35 +0530

    Update bloodAvailabilityRoutes.js

--- bloodAvailabilityRoutes.js
+++ bloodAvailabilityRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAvailableDonors } = require('../controllers/bloodAvailabilityController');
+const { getAvailableDonors } = require('../bloodAvailabilityController');
 
 // Define route for getting available donors by blood group
 router.get('/', getAvailableDonors); // Route to get available donors
